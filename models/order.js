5f79fe03d06f8bf84680084bf3d71e3fe687ce4c
username <youremail@domain.com>
2022-09-26 12:56:26 -0400

    add meal images

--- models/order.js
+++ models/order.js
@@ -19,4 +19,44 @@ const orderSchema = new mongoose.Schema({
       },
     },
   ],
-});
+  anyAllergies: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  DeliveryAddress: {
+    address: { type: String, required: false},
+    city: { type: String, required: false},
+    postalCode: { type: String, required: false},
+
+  },
+  paymentMethod: {
+    type: String,
+    required: true
+  },
+  paymentResult: {
+    id: { type: String },
+    status: { type: String},
+    update_time: { type: String},
+    email_address: { type: String},
+  },
+  taxPrice: {
+    type: Number,
+    required: true,
+    default: 0.0
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0.0
+  },
+  isPaid: {
+    type: Boolean,
+    required: true,
+    default: false
+  }, 
+}, {
+    timestamps: true
+  });
+
+module.exports = mongoose.model('Order', orderSchema)
