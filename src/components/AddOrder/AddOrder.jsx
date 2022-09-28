530233465831d67760cbac7ccf0a3f396f7ec643
username <youremail@domain.com>
2022-09-28 14:18:49 -0400

    Polishing AddOrder Component

--- src/components/AddOrder/AddOrder.jsx
+++ src/components/AddOrder/AddOrder.jsx
@@ -1,8 +1,59 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 
 
-export default function AddOrderForm(){
+export default function AddOrderForm(props){
+    // create the state, pay attention to how the inputs are setup
+
+
+    //The function that handles the changes on the input, look at the inputs for the name of it
+
+    function handleSubmit(e){
+
+    }
+
+
+
     return(
-        <span>Order Form</span>
+        <Form onSubmit={handleSubmit}>
+            <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridName">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="name" placeholder="Enter First Name" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridName">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="name" placeholder="Enter Last Name" />
+                </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="formGridAddress">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formGridAddress2">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="Apartment/Studio/Floor" />
+            </Form.Group>
+
+            <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridCity">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control />
+                </Form.Group>
+            </Row>
+
+            <Button variant="primary" type="submit"> Place Order </Button>
+        </Form>
+       
     )
 }
--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-
+import AddOrder from '../../components/AddOrder/AddOrder';
 
 
 export default function OrderPage(){
     return(
         <div>
             <h1>Place your orders here!</h1>
+            <AddOrder />
+            
         </div>
     )
 }
